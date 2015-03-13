7a269bd1cbe7fc9342a1d8450d24085d7fd1de08
Zoe <doticus@gmail.com>
2015-03-13 16:16:55 -0400

    Modular

---
+++ learnyounode/modular.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+var path = require('path');
+
+var data = fs.readdir(fileList)
+
+module.exports = function(fileList, extension, (err, data))
+fs.readdir(fileDir, function(err,directory) {
+  for (i = 0; i < directory.length; i++) {
+    if (path.extname(directory[i]) === filterBy) {
+      console.log(directory[i]);
+  }
+}
+});
+
+exports
+function fileExtensions(process.argv[2], process.argv[3], function(err,))
---
+++ learnyounode/program.js
@@ -0,0 +1,4 @@
+var modular = require("./modular")
+
+var fileList = process.argv[2];
+var extension = process.argv[3];
